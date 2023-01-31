e4f128221dfc74e5a8a3751340ed3c094e0e5ae4
acd02 <alexandre.cadiot@adevinta.com>
2023-01-31 17:18:54 +0100

    fix: imports

--- handleThemeConfiguration.ts
+++ handleThemeConfiguration.ts
@@ -1,5 +1,5 @@
-import { createCSSTokens, createTailwindConfig } from './packages/theme/dist/index.mjs'
-import { themes } from './src/themes'
+import { createCSSTokens, createTailwindConfig } from './packages/theme/src/index.mjs'
+import { themes } from './src/themes/index.js'
 
 createTailwindConfig('./tailwind.theme.conf.cjs')
 createCSSTokens('./src/tailwind.css', themes)
